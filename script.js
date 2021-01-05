d92bd66963815359a3d729eb6aad09f62176e9a5
ahtsho <tsegaye.ahadu@gmail.com>
2021-01-05 15:53:03 +0100

    robot svg extracted

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  var actionStack = [];
- var asteroidPositions = [[0,0]]
+ var asteroidPositions = [[0,0,"robot"]]
+ 
  function updateTranslate(x,y){
 	next = getNextPos(x,y)
 	if(next[0]>0 && next[0]<500){
@@ -13,6 +14,35 @@
 	}
 	removeAllBouncesFromBoard()
  }
+ function extractDirection(pt){
+ 	p = []
+ 	p[0] = pt[0] - 52
+ 	p[1] = pt[1] - 17
+ 	if(p[0]>0){
+ 		p[0] = p[0]/100
+ 	}
+ 	if(p[1]>0){
+ 		p[1] = p[1]/100
+ 	}
+ 	return p
+ }
+ function updateTranslate2(x,y){
+	next = getNextPos(x,y)
+	if(next[0]>0 && next[0]<500){
+		if(next[1]>0 && next[1]<500){
+			//"translate(152px, 117px) rotate(50deg)"
+			if(isEmptyCell(extractDirection(next),true)){
+				setTransformation('translate('+next[0]+'px,'+next[1]+'px) '+getCurrentRotate())
+			} else {
+				shake(extractDirection(next))
+			}
+		} else {
+			addVBounce()
+		}
+	} else {
+		addHBounce()
+	}
+ }
  function addVBounce(){
  	removeAllBouncesFromBoard()
  	addBounceToBoard('v')
@@ -22,11 +52,14 @@
  	addBounceToBoard('h')
  }
  function removeAllBouncesFromBoard(){
- 	document.getElementById("board").classList.remove("bounce-v")
- 	document.getElementById("board").classList.remove("bounce-h")
+ 	document.getElementById("maze").classList.remove("bounce-v")
+ 	document.getElementById("maze").classList.remove("bounce-h")
  }
  function addBounceToBoard(dir){
- 	document.getElementById("board").classList.add("bounce-"+dir)
+ 	document.getElementById("maze").classList.add("bounce-"+dir)
+ }
+ function resetMazeClass(){
+ 	document.getElementById("maze").classList.add("coding")
  }
  function getNextPos(x,y){
  	currXY = getCurrentXYval()
@@ -39,6 +72,14 @@
 	nextD = (getCurrentDval() + d)%360
 	setAttribute(getCurrentTranslate()+' rotate('+nextD+',50,50)')
  }
+ function updateRotate2(d){
+	setTransformation(getCurrentTranslate()+' rotate('+(getCurrentDval() + d)%360+'deg)')
+ }
+ function setTransformation(val){
+ 	//val is like "translate(152px, 117px) rotate(50deg)"
+ 	document.getElementById("robot").style.transform= val
+ 	document.getElementById("robot").classList.add("slide")
+ }
  function setAttribute(val){
 	document.getElementById("robot").setAttribute('transform',val)
 	document.getElementById("robot").classList.add("slide")
@@ -47,25 +88,32 @@
  function getCurrentTransform(){
 	return document.getElementById("robot").getAttribute('transform')
  }
+ function getCurrentTransformFromStyle(){
+ 	return document.getElementById("robot").style.transform
+ }
  function getCurrentXYval(){
-	xy = getCurrentTransform().split(" ")[0].replace(/translate/g,"").replace(/\(|\)/g, "").split(",")
+	//xy = getCurrentTransform().split(" ")[0].replace(/translate/g,"").replace(/\(|\)/g, "").split(",")
+	xy = getCurrentTransformFromStyle().replace(/translate|\(|\)|px|,/g,"").split(" ")
 	xy[0] = Number(xy[0])
 	xy[1] = Number(xy[1])
 	return xy
  }
  function getCurrentTranslate(){
-	return getCurrentTransform().split(" ")[0]
+	//return getCurrentTransform().split(" ")[0]
+	return getCurrentTransformFromStyle().split("rotate")[0]
  }
  function getCurrentRotate(){
-	return getCurrentTransform().split(" ")[1]
+	//return getCurrentTransform().split(" ")[1]
+	return getCurrentTransformFromStyle().split("px)")[1]
  }
  function getCurrentDval(){
-	return Number(getCurrentTransform().split(" ")[1].replace(/rotate/g,"").replace(/\(|\)/g, "").split(",")[0])
+	//return Number(getCurrentTransform().split(" ")[1].replace(/rotate/g,"").replace(/\(|\)/g, "").split(",")[0])
+	return Number(getCurrentRotate().replace(/\(|\)|rotate|deg/g, ""))
  }
  function move(){
 	currOrientation = getCurrentDval()
 	step = getNextStep()
-	updateTranslate(step[0],step[1])
+	updateTranslate2(step[0],step[1])
  }
  function getNextStep(){
  	step = []
@@ -86,10 +134,24 @@
 	return step
  }
  function turnR(){
-	updateRotate(90)
+	updateRotate2(90)
  }
  function turnL(){
-	updateRotate(-90)
+	updateRotate2(-90)
+ }
+ function shake(pt){
+ 	document.getElementById(getAsteroidIdByPosition(pt)).classList.add("shake")
+ }
+ function shake(pt){
+ 	document.getElementById(getAsteroidIdByPosition(pt)).classList.add("shake")
+ }
+ function getAsteroidIdByPosition(pt){
+ 	for (var i = 0; i < asteroidPositions.length; i++) {
+ 		if(asteroidPositions[i][0]==pt[0] && asteroidPositions[i][1]==pt[1]){
+ 			return asteroidPositions[i][2];
+ 		}
+ 	}
+ 	return undefined
  }
  function moveToCharge(){
  	step = getNextStep()
@@ -156,18 +218,19 @@ function getRandomInt(max) {
  		document.getElementById("pila-conta").setAttribute('transform','translate('+randX+','+randY+')')
  	}
  }
- function getUniqueTransformedPoints(max,bx,by){
- 	pt = genNonOverlappingPt(max)
+ function getUniqueTransformedPoints(max,bx,by,id){
+ 	pt = genNonOverlappingPt(max,id)
  	ptT = []
  	ptT[0] = pt[0]*100+bx
  	ptT[1] = pt[1]*100+by
  	return ptT
  }
   
- function genNonOverlappingPt(max){
+ function genNonOverlappingPt(max,id){
  	pt = []
  	pt[0] = getRandomInt(max)
  	pt[1] = getRandomInt(max)
+ 	pt[2] = id
  	inserted = insertIntoTakenPositions(pt)
  	while(!inserted){
  		pt[0] = getRandomInt(max)
@@ -176,34 +239,41 @@ function getRandomInt(max) {
  	}
  	return pt
  }
- /*
- function genNonOverlappingPts(max,bx,by){
- 	pt = genRandomPt(max,bx,by)
- 	inserted = insertIntoTakenPositions(pt)
- 	while(!inserted){
- 		pt = genRandomPt(max,bx,by)
- 		inserted = insertIntoTakenPositions(pt)
- 	}
- 	return pt
- }*/
- function insertIntoTakenPositions(pt){
- 	for (var i = 0; i < asteroidPositions.length; i++) {
- 		if(asteroidPositions[i][0]==pt[0] && asteroidPositions[i][1]==pt[1]){
- 			return false;
+function isEmptyCell(pt, allow){
+	for (var i = 0; i < asteroidPositions.length; i++) {
+ 		if(asteroidPositions[i][0]==pt[0] && asteroidPositions[i][1]==pt[1] ){
+ 			//the obstacles are for now only asteroids
+ 			//&& asteroidPositions[i][2].search("aster")==0){
+ 			if(asteroidPositions[i][2].search("pila")==0 && allow){
+ 				return true;
+ 			} else {
+ 				return false;
+ 			}
  		}
  	}
- 	asteroidPositions.push(pt)
  	return true;
+}
+ function insertIntoTakenPositions(pt){
+ 	isEmpty = isEmptyCell(pt,false)
+ 	if (isEmpty){
+ 		asteroidPositions.push(pt)
+ 	}
+ 	return isEmpty;
  }
  function placeAsteroidsAndBatteryRand(){
  	for (var i = 1; i <=4; i++) {
- 		pt = getUniqueTransformedPoints(5,58,140)
+ 		pt = getUniqueTransformedPoints(5,58,140,"aster"+i)
  		placeObjectById("aster"+i,pt[0],pt[1]);
  	}
- 	pt = getUniqueTransformedPoints(5,93,150)
+ 	pt = getUniqueTransformedPoints(5,93,150,"pila")
  	placeObjectById("pila",pt[0],pt[1])
  }
  function placeObjectById(id,x,y){
- 	document.getElementById(id).style.left=x+"px"
+	document.getElementById(id).style.left=x+"px"
 	document.getElementById(id).style.top=y+"px"
+ }
+ function removeShakeFromAllObjects(){
+ 	for (var i = 0; i < asteroidPositions.length; i++) {
+ 		document.getElementById(asteroidPositions[i][2]).classList.remove("shake")
+ 	}
  }
